866c7238303f5df0fba9a8efea7c5da68e6336c2
Bundy <94148798+TheFantomZone@users.noreply.github.com>
2022-02-07 01:18:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NFT Collectible Assessment
+
+This is how far I got, which isn't far. The ABI code both initial and deployed are included in the JSON file. The smart contract with the requested parameters is deployed to the Ropsten ETH testnetwork under the name BundyAssessment. The connect wallet button is replaced with the mint button upon succesful linkage, after reloading the site it's automatically detected that a valid previously connected wallet and it will show the mint button instead. 
+
+App.js includes only a fraction of the functions from my smart contract and I'm not skilled enough yet to display them in the front-end, but am able to use them from the back-end. 
---
+++ src/App.js
@@ -0,0 +1,109 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import contract from './contracts/BundyAssessment.json';
+import { ethers } from 'ethers';
+
+const contractAddress = "0x6c33213249BAb53415d07EdEB9dC76e432C9CF87";
+const abi = contract.abi;
+
+function App() {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet detected! We gaan ervoor")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const mintNftHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        let nftTxn = await nftContract.mintNFTs(1, { value: ethers.utils.parseEther("0.01") });
+
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://ropsten.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+        Mint it!
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
+  return (
+    <div className='main-app'>
+      <h1>Bundys Assessment</h1>
+      <div>
+        
+        {currentAccount ? mintNftButton() : connectWalletButton()}
+      </div>
+      <br></br>
+      <a href="https://imgur.com/Xp67GPs"><img src="https://i.imgur.com/Xp67GPs.jpg" title="source: imgur.com" /></a>
+    </div>
+  )
+}
+
+export default App;
